c3f73afa7e4bafe7d4a83ab125150e4865a42b83
nicol2021 <wolna2011@gmail.com>
2022-05-07 22:28:54 +0300

    decorator and others, section 1, last lesson

---
+++ ustaval.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kqnfdgdfgdfggf786hfg6hfg6h7fa'
+
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}]
+
+@app.route('/')
+def index():
+   return render_template('index.html', menu=menu)
+
+@app.route("/about")
+def about():
+   print( url_for('about') )
+   return render_template('about.html', title="О сайте", menu=menu)
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+   if request.method == 'POST':
+      # print(request.form)
+      # print(request.form['username'])
+      if len(request.form['username']) > 2:
+         flash('Сообщение отправлено', category='success')
+      else:
+         flash('Ошибка отправки', category='error')
+   return render_template('contact.html', title="Обратная связь", menu=menu)
+
+# # @app.route("/profile/<username>")
+# @app.route("/profile/<username>")
+# # def profile(username, path):
+# #    return f"Пользователь: {username}, {path}"
+# def profile(username):
+#    return f"Профиль пользователя: {username}"
+
+@app.route("/profile/<username>")
+def profile(username):
+   if 'userLogged' not in session or session['userLogged'] != username:
+      abort(401)
+   return f"Пользователь: {username}"
+
+# with app.test_request_context():
+#    print( url_for('index') )
+#    print( url_for('about') )
+#    print( url_for('profile', username="hxxx") )
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+   if 'userLogged' in session:
+      return redirect(url_for('profile', username=session['userLogged']))
+   elif request.method=='POST' and request.form['username'] == "selfedu" and request.form['psw'] == "123":
+      session['userLogged'] = request.form['username']
+      return redirect(url_for('profile', username=session['userLogged']))
+ 
+   return render_template('login.html', title="Авторизация", menu=menu)
+
+@app.errorhandler(404)
+def pageNotFount(error):
+   return render_template('page404.html', title="Страница не найдена", menu=menu), 404
+
+if __name__ == '__main__':
+   # app.run(threaded=True, port=5000)
+   app.run(debug=True)
